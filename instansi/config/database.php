ac5999c20254bacaeb41a86365bc6b88bb1e5ad7
Desa Garut <desagarut.net@gmail.com>
2022-10-25 16:56:37 +0700

    Fixbug

--- instansi/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'sim_sthgarut';
-
-$db['default']['stricton'] = TRUE;
-?>
