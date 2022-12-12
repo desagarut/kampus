2117675898aa0edb00325c790f9e629e0fa28205
Desa Garut <desagarut.net@gmail.com>
2022-12-12 12:02:11 +0700

    Update Tema Kampus

--- themes/kampus/layouts/artikel.tpl.php
+++ themes/kampus/layouts/artikel.tpl.php
@@ -5,15 +5,8 @@
 <body>
 <?php $this->load->view($folder_themes . '/commons/spinner.php') ?>
 <?php $this->load->view($folder_themes . '/commons/nav.php') ?>
-<?php //$this->load->view($folder_themes .'/partials/carousel') ?>
 
 <?php $this->load->view($folder_themes .'/partials/artikel') ?>
-<?php $this->load->view($folder_themes .'/partials/about') ?>
-<?php $this->load->view($folder_themes .'/partials/categories') ?>
-<?php $this->load->view($folder_themes .'/partials/courses') ?>
-<?php $this->load->view($folder_themes .'/partials/team') ?>
-<?php $this->load->view($folder_themes .'/partials/testimonial') ?>
-
 </body>
 <?php $this->load->view($folder_themes . '/commons/footer') ?>
 
