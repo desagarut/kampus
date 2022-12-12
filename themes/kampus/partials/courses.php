150ba03c9cbccac88115e9457a0b0dea5ea346b9
Desa Garut <desagarut.net@gmail.com>
2022-12-12 15:30:37 +0700

    Update

--- themes/kampus/partials/courses.php
+++ themes/kampus/partials/courses.php
@@ -4,8 +4,8 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Courses</h6>
-                <h1 class="mb-5">Popular Courses</h1>
+                <h6 class="section-title bg-white text-center text-primary px-3">Program Studi</h6>
+                <h1 class="mb-5">Program Studi</h1>
             </div>
             <div class="row g-4 justify-content-center">
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
