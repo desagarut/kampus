0e00f9725352afcb6e4f631ee96e4ab02b48e45f
Desa Garut <desagarut.net@gmail.com>
2022-12-12 09:43:20 +0700

    Update team.php

--- themes/kampus/partials/team.php
+++ themes/kampus/partials/team.php
@@ -1,6 +1,6 @@
 
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<?php if(is_file($file_gambar)) : ?>
+<?php if(is_file($aparatur_desa)) : ?>
 <!-- Team Start -->
 <div class="container-xxl py-5">
     <div class="container">
