3cb24e924ea70ef9a1e0f9da7fb2a69561475f0f
Desa Garut <desagarut.net@gmail.com>
2022-12-12 15:03:44 +0700

    Update

--- themes/kampus/partials/team.php
+++ themes/kampus/partials/team.php
@@ -4,8 +4,8 @@
 <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h6 class="section-title bg-white text-center text-primary px-3">Dosen & Pengelola <?= ucfirst($this->setting->website_title)?></h6>
-            <h1 class="mb-5">SDM Berpengalaman</h1>
+            <h6 class="section-title bg-white text-center text-primary px-3">S D M</h6>
+            <h1 class="mb-5">Dosen & Pengelola</h1>
         </div>
         <div class="row g-4">
         <?php foreach($aparatur_desa['daftar_perangkat'] as $data) : ?>
