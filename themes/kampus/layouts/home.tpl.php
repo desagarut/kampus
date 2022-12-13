951dfeae1b9200383897835d41ceb6216df3fcf9
Desa Garut <desagarut.net@gmail.com>
2022-12-13 13:03:29 +0700

    Update home.tpl.php

--- themes/kampus/layouts/home.tpl.php
+++ themes/kampus/layouts/home.tpl.php
@@ -4,6 +4,6 @@
 <?php $this->load->view($folder_themes .'/partials/service') ?>
 <?php $this->load->view($folder_themes .'/partials/about') ?>
 <?php $this->load->view($folder_themes .'/partials/categories') ?>
-<?php $this->load->view($folder_themes .'/partials/courses') ?>
+<?php //$this->load->view($folder_themes .'/partials/courses') ?>
 <?php $this->load->view($folder_themes .'/partials/team') ?>
 <?php $this->load->view($folder_themes .'/partials/testimonial') ?>
